0e9096d98e7487df056c6612933c2990313a7d86
JingyZhu <jingyz@umich.edu>
2019-12-06 02:46:25 -0500

    forgot crawl

--- utils/crawl.py
+++ utils/crawl.py
@@ -8,9 +8,9 @@ import time
 from os.path import abspath, dirname, join
 import base64
 
-def chrome_crawl(url, timeout=120, screenshot=False):
+def chrome_crawl(url, timeout=120, screenshot=False, ID=''):
     try:
-        cur = str(int(time.time())) + '_' + str(os.getpid()) 
+        cur = str(int(time.time())) + '_' + str(os.getpid()) + ID
         file = cur + '.html'
         cmd = ['node', join(dirname(abspath(__file__)), 'run.js'), url, '--filename', cur]
         if screenshot:
@@ -21,16 +21,18 @@ def chrome_crawl(url, timeout=120, screenshot=False):
         pid = open(file, 'r').read()
         call(['kill', '-9', pid])
         os.remove(file)
-        return "", ""
+        return "" if not screenshot else "", ""
     html = open(file, 'r').read()
     os.remove(file)
+    if not screenshot:
+        return html
+
     img = open(cur + '.jpg', 'r').read()
     os.remove(cur + '.jpg')
-    if screenshot:
-        url_file = url.replace('http://', '')
-        url_file = url_file.replace('https://', '')
-        url_file = url_file.replace('/', '-')
-        f = open(url_file + '.jpg', 'wb+')
-        f.write(base64.b64decode(img))
-        f.close()
+    url_file = url.replace('http://', '')
+    url_file = url_file.replace('https://', '')
+    url_file = url_file.replace('/', '-')
+    f = open(url_file + '.jpg', 'wb+')
+    f.write(base64.b64decode(img))
+    f.close()
     return html, url_file + 'jpg'
