453a6b2a2dd04a5eab221d968e7be331e48b62eb
JingyZhu <jingyz@umich.edu>
2019-12-12 17:36:28 -0500

    wayback machine for crawling all the links in certain years

--- utils/crawl.py
+++ utils/crawl.py
@@ -7,6 +7,8 @@ import os
 import time
 from os.path import abspath, dirname, join
 import base64
+import threading
+import itertools
 
 def chrome_crawl(url, timeout=120, screenshot=False, ID=''):
     try:
@@ -61,8 +63,61 @@ def wayback_index(url, param_dict={}):
         return r, "Success",
     else:
         return [], "Empty"
-    
 
+
+
+def wayback_year_links(prefix, years, NUM_THREADS=10):
+    """
+    Get the result of links in certain years
+    prefix: some string of url
+    years: list of years which would be query
+
+    Should be add in try catch. In case of connection error
+    """
+    total_r = {}
+    wayback_home = 'http://web.archive.org/web/'
+    params = {
+        'output': 'json',
+        'url': prefix,
+        "limit": '100000',
+        'collapse': 'urlkey',
+        'filter': ['statuscode:200', 'mimetype:text/html'],
+    }
+    l = threading.Lock()
+    def get_half_links(year, half):
+        nonlocal total_r
+        total_r.setdefault(year, set())
+        params.update({
+            "from": "{}{}01".format(year, str(half*6).zfill(2)),
+            "to": "{}{}31".format(year, str(half*6+6).zfill(2))
+            # 'collapse': 'timestamp:4',
+        })
+        
+        r = requests.get('http://web.archive.org/cdx/search/cdx', params=params)
+        r = r.json()
+        
+        r = [u[2] for u in r[1:]]
+        assert(len(r) < 100000 )
+        l.acquire()
+        for url in r:
+            total_r[year].add(url)
+        l.release()
+    t = []
+    year_half_orig = list(itertools.product(years, [0, 1]))
+    year_half = []
+    for begin in range(0, len(year_half_orig), NUM_THREADS):
+        end = begin + NUM_THREADS  if begin + NUM_THREADS < len(year_half_orig) else len(year_half_orig) 
+        year_half.append(year_half_orig[begin:end])
+    for shot in year_half:
+        print(shot)
+        t = []
+        for year, month in shot:
+            t.append(threading.Thread(target=get_half_links, args=(year, month)))
+            t[-1].start()
+        for ti in t:
+            ti.join()
+
+    return {k: list(v) for k, v in total_r.items()}
 
 
     
