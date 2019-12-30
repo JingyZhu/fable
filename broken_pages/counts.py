3ed82362350f3fa7a60d3b70a24870552d9af8bd
JingyZhu <jingyz@umich.edu>
2019-12-29 19:24:36 -0500

    Try markdown

--- broken_pages/counts.py
+++ broken_pages/counts.py
@@ -9,3 +9,29 @@ sys.path.append('../')
 import config
 from utils import plot
 
+db = MongoClient(config.MONGO_HOSTNAME).web_decay
+year = 1999
+
+def status_by_host():
+    """
+    Create collections as year, host, status (one KXX), and detail
+    """
+    db.host_status.create_index([('hostname', pymongo.ASCENDING), ('year', pymongo.ASCENDING),\
+         ('status', pymongo.ASCENDING), ('detail', pymongo.ASCENDING)], unique=True)
+    for url in db.url_status.find({'year': year}):
+        status = url['status']
+        status = str(int(status) // 100) + "xx" if status != 'DNSError' and status != 'OtherError' else status
+        if status[0] not in ['2', '3', '4', '5']:
+            continue
+        try:
+            db.host_status.insert_one({
+                "hostname": url['hostname'],
+                "year": year,
+                "status": status,
+                'detail': url['detail']
+            })
+        except:
+            continue
+
+status_by_host()
+        
---
+++ db_format.md
@@ -0,0 +1,11 @@
+
+#### url_sample
+Sampled urls. For every host with >= urls, sample 100 from them
+```json
+{
+    "_id": url
+    "url": string,
+    "hostname": string,
+    "year": int
+}
+```
