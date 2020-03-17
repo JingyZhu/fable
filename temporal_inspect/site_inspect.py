9d18c6604e7216c94f243d12f72ed3c69deff7f4
JingyZhu <jingyz@umich.edu>
2020-03-17 13:42:09 -0400

    Make collect tech of site available

--- temporal_inspect/site_inspect.py
+++ temporal_inspect/site_inspect.py
@@ -22,31 +22,52 @@ db_test = MongoClient(config.MONGO_HOSTNAME).wd_test
 PS = crawl.ProxySelector(config.PROXIES)
 
 
+def same_tech(a, b):
+    a = {k: sorted(v) for k, v in a.items()}
+    b = {k: sorted(v) for k, v in b.items()}
+    return a == b
+
+
 def subhost_tech_change(subhost):
-    homepage_snapshots, _ = crawl.wayback_index(subhost, total_link=True, param_dict={"filter": "statuscode:200"})
+    homepage_snapshots, _ = crawl.wayback_index(subhost, total_link=True, param_dict={"filter": "statuscode:200"}, proxies=PS.select())
     homepage_snapshots = sorted(homepage_snapshots, key=lambda x: int(x[0]))
-    begin_idx, end_idx = 0, len(homepage_snapshots)-1
+    begin_idx, end_idx = 0, len(homepage_snapshots) - 1
     begin_ts, end_ts = homepage_snapshots[0][0], homepage_snapshots[-1][0]
-    def check_transfer(begin_idx, end_idx, records):
+    def wappalyzer_once(obj, visits):
+        """Only crawl certain ts once across all search"""
+        ts, url, _ = obj
+        if ts in visits: return visits[ts]
+        tech = crawl.wappalyzer_analyze(url)
+        visits[ts] = tech
+        return tech
+
+    def check_transfer(begin_idx, end_idx, records, visits):
         transfer = []
+        print('period:', begin_idx, end_idx)
         if begin_idx == end_idx:
             return transfer
-        begin_tech = crawl.wappalyzer_analyze(records[begin_idx][1])
-        end_tech = crawl.wappalyzer_analyze(records[end_idx][1])
-        if begin_tech == end_tech: 
+        begin_tech = wappalyzer_once(records[begin_idx], visits)
+        end_tech = wappalyzer_once(records[end_idx], visits)
+        if same_tech(begin_tech, end_tech): # TODO Not necessarily correct
             return transfer
         elif end_idx - begin_idx <= 1:
             return [(records[begin_idx][0], begin_tech), (records[end_idx][0], end_tech)]
         else:
-            transfer = check_transfer(begin_idx, (begin_idx + end_idx) // 2, records) \
+            transfer = check_transfer(begin_idx, (begin_idx + end_idx) // 2, records, visits) \
                        + transfer \
-                       + check_transfer((begin_idx + end_idx) // 2, end_idx, records)
+                       + check_transfer((begin_idx + end_idx) // 2, end_idx, records, visits)
             return transfer
-    tech_transfer_records = check_transfer(begin_idx, end_idx, homepage_snapshots)
+    visits = {}
+    tech_transfer_records = check_transfer(begin_idx, end_idx, homepage_snapshots, visits)
     # TODO: Finish this function to determine period of techs AND FIND WAY TO DEFINE TWO DICT AS SAME!
+    tech_transfer_records = [(begin_ts, visits[begin_ts])] + tech_transfer_records + [(end_ts, visits[end_ts])]
+    periods = [{
+        "startTS": tech_transfer_records[i][0],
+        "endTS": tech_transfer_records[i+1][0],
+        "tech": tech_transfer_records[i][1]
+    } for i in range(0, len(tech_transfer_records, 2))]
+    return periods
     # for tech_transfer in tech_transfer_records:
-
-
 
 
 def collect_tech_change_sites():
@@ -55,6 +76,10 @@ def collect_tech_change_sites():
     """
     subhosts = db.site_tech.find()
     subhosts = list(subhosts)
-    for subhost in subhosts:
+    for subhost in subhosts[:5]:
+        print(subhost)
+        period = subhost_tech_change(subhost['_id'])
+        print(json.dumps(period, indent=2))
 
+collect_tech_change_sites()
     
