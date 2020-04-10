976f917988db8bd0710e568b3f4ccfdec694c2dc
JingyZhu <jingyz@umich.edu>
2020-04-10 02:51:05 -0400

    Add SiteTree for plot tree of a site. Generation part finished

--- utils/plotly_utils.py
+++ utils/plotly_utils.py
@@ -4,6 +4,65 @@ Plot utilities for plotly libraries
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
+from urllib.parse import urlparse
+from collections import defaultdict
+import re
+import os
+import json
+
+class SiteTree:
+    """
+    Generate a tree from the given urls
+    The intermediate node will be elimiated if there is only one child (leaf is exception)
+    Use plotly to plot the tree
+    """
+    def generate_tree(self):
+        tree = lambda: defaultdict(tree)
+        url_tree = tree()
+        # Insert urls into 
+        for obj in self.urls:
+            url, status, broken = obj['url'], obj['status'], obj.get('sic_broken')
+            up = urlparse(url)
+            subhost, path, query = up.netloc.split(':')[0], up.path, up.query
+            if path == '': path += '/'
+            directories = [subhost] + path.split('/')[1:]
+            cur_node = url_tree
+            for d in directories[:-1]:
+                cur_node = cur_node[d]
+            filename = directories[-1] + '?' + query if query else directories[-1]
+            cur_node[filename] = [status, broken]
+        # Delete intermediate nodes with only one child (link list)
+        json.dump(url_tree, open('tmp/tree_pre.json', 'w+'))
+        stack = [url_tree]
+        while len(stack) > 0:
+            cur_node = stack.pop()
+            if isinstance(cur_node, list): continue
+            keys = list(cur_node.keys())
+            for k in keys:
+                cur_k, v = k, cur_node[k]
+                need_cut = False
+                while not (len(v) > 1 or isinstance(v, list)):
+                    sub_k, v = list(v.keys())[0], list(v.values())[0]
+                    cur_k = '{}/{}'.format(cur_k, sub_k)
+                    cur_node[cur_k] = v
+                    need_cut = True
+                if need_cut: del(cur_node[k])
+                stack.append(cur_node[cur_k])
+        json.dump(url_tree, open('tmp/tree.json', 'w+'))
+        return url_tree
+
+    def update_urls(self, urls):
+        """urls should include status and broken metadata (refer to db_format-->url_status_implicit_broken)"""
+        self.urls = urls
+        self.tree = self.generate_tree()
+
+    def __init__(self, urls):
+        self.update_urls(urls)
+    
+    def plot_tree(self):
+        # TODO imlement this function
+        pass
+
 
 def plot_CDF(df, xtitle="", ytitle="", title="", cut=1, clear_bound=True):
     """
