f6ac6c71c89e91c97e66a90adce9c2a636d55bd7
JingyZhu <jingyz@umich.edu>
2020-10-17 17:23:38 -0400

    separate data with code

--- fable/config.py
+++ fable/config.py
@@ -31,9 +31,11 @@ default_var_dict = var_dict.copy()
 def config(key):
     return var_dict.get(key)
 
+
 def set_var(key, value):
     exec(f'{key.upper()} = {value}', globals())
     exec(f'{var_dict[key.upper()]} = {value}', globals())
+
 
 def unset(key):
     # TODO: Also unset the defined variables
@@ -41,12 +43,6 @@ def unset(key):
         del(var_dict[key])
     except: pass
 
-def apply_custom(key):
-    custom_yml = yaml.load(open(os.path.join(os.path.dirname(__file__), 'custom_setups.yml'), 'r'), Loader=yaml.FullLoader)
-    setups = custom_yml.get(key)
-    if setups is None: return
-    for k, v in setups.items():
-        set_var(k, v)
 
 def back_default():
     global var_dict
