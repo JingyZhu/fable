d88e923fad694f703043b4af26b8aa0aaa300afa
Jingyuan <jingyz@umich.edu>
2021-06-03 13:42:29 -0400

    update netloc_dir to exclude www

--- fable/utils/url_utils.py
+++ fable/utils/url_utils.py
@@ -340,9 +340,12 @@ def common_prefix_diff(dest, src):
 
 
 def netloc_dir(url):
+    """Get host, nondigit_dirname for a URL"""
     url = filter_wayback(url)
     us = urlsplit(url)
     p = us.path
     if len(p) > 1 and p[-1] == '/': p = p[:-1]
     if p == '':  p == '/'
-    return (us.netloc.split(':')[0], nondigit_dirname(p))
+    hosts = us.netloc.split(':')[0].split('.')
+    if 'www' in hosts[0]: hosts = hosts[1:]
+    return ('.'.join(hosts), nondigit_dirname(p))
